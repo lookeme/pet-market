6f154f342d2d822eb6a69514bc2f0aa9ddc4ba53
Денис Белокопытов <dvabelokopytov@sberbank.ru>
2024-06-17 22:42:58 +0300

    add logging on client

--- internal/integration/accural_integration.go
+++ internal/integration/accural_integration.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"pet-market/internal/logger"
 	"time"
+
+	"go.uber.org/zap"
 )
 
 var url = "/api/orders/"
@@ -24,13 +26,15 @@ func New(host string) *AccuralIntegration {
 }
 
 func (a *AccuralIntegration) GetOrder(orderNumber string, timeout time.Duration) (*OrderAccural, error) {
-
+	a.log.Log.Info("create request to accural...")
 	req, err := http.NewRequest(http.MethodGet, a.host+url+orderNumber, nil)
 	if err != nil {
+		a.log.Log.Error(err.Error())
 		return nil, err
 	}
 	res, getErr := a.Client.Do(req)
 	if getErr != nil {
+		a.log.Log.Error(err.Error())
 		return nil, getErr
 	}
 
@@ -40,14 +44,18 @@ func (a *AccuralIntegration) GetOrder(orderNumber string, timeout time.Duration)
 
 	body, readErr := io.ReadAll(res.Body)
 	if readErr != nil {
+		a.log.Log.Error(readErr.Error())
 		return nil, readErr
 	}
-
 	order := OrderAccural{}
 	jsonErr := json.Unmarshal(body, &order)
 	if jsonErr != nil {
 		return nil, jsonErr
 	}
+	a.log.Log.Info("return order",
+		zap.String("order", order.Order),
+		zap.String("status", order.Status),
+		zap.Int("accural", order.Accrual))
 	return &order, nil
 }
 

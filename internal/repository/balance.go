1c65e829c0222b830d7462a94153ba54091246fa
Денис Белокопытов <dvabelokopytov@sberbank.ru>
2024-06-18 22:15:03 +0300

    fix bug balance

--- internal/repository/balance.go
+++ internal/repository/balance.go
@@ -17,7 +17,7 @@ func NewBalanceRepository(pg *Postgres) *BalanceRepositoryImpl {
 
 func (b *BalanceRepositoryImpl) GetBalance(ctx context.Context, userID int) (api.Balance, error) {
 	var balance api.Balance
-	sqlStatement := "SELECT current, withdrawn FROM (SELECT SUM(accrual) as current, user_id FROM orders WHERE user_id = $1 group by user_id) T1 INNER JOIN (SELECT SUM(sum) as withdrawn, user_id FROM withdrawals WHERE user_id = $1 group by user_id) T2 ON T1.user_id=T2.user_id;"
+	sqlStatement := "SELECT Coalesce(current, 0) as current,  Coalesce(withdrawn, 0) as withdrawn FROM (SELECT SUM(accrual) as current, user_id FROM orders WHERE user_id = $1 group by user_id) T1 LEFT JOIN (SELECT SUM(sum) as withdrawn, user_id FROM withdrawals WHERE user_id = $1 group by user_id) T2 ON T1.user_id = T2.user_id;"
 	err := b.pg.СonPool.QueryRow(ctx, sqlStatement, userID).Scan(&balance.Current, &balance.Withdrawn)
 	return balance, err
 }
